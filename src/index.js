0e8610640f3353a0a5ec931c5c3d8ba2dc0c1024
Alexander Teinum <ateinum@gmail.com>
2018-11-25 00:14:31 +0100

    Adjust color usages

--- src/index.js
+++ src/index.js
@@ -21,23 +21,23 @@ function generateColorTheme(colors) {
   return {
     BackgroundColour: r(colors.blueGray[0]),
     Black: r(colors.blueGray[0]),
-    Blue: r(colors.blue[18]),
-    BoldBlack: r(colors.blueGray[18]),
-    BoldBlue: r(colors.blue[26]),
-    BoldCyan: r(colors.cyan[26]),
-    BoldGreen: r(colors.green[26]),
-    BoldMagenta: r(colors.purple[26]),
-    BoldRed: r(colors.red[26]),
+    Blue: r(colors.blue[26]),
+    BoldBlack: r(colors.blueGray[26]),
+    BoldBlue: r(colors.blue[34]),
+    BoldCyan: r(colors.cyan[34]),
+    BoldGreen: r(colors.green[34]),
+    BoldMagenta: r(colors.purple[34]),
+    BoldRed: r(colors.red[34]),
     BoldWhite: r(colors.blueGray[39]),
     BoldYellow: r(colors.orange[33]),
     CursorColour: r(colors.blueGray[28]),
-    Cyan: r(colors.cyan[18]),
+    Cyan: r(colors.cyan[26]),
     ForegroundColour: r(colors.blueGray[34]),
-    Green: r(colors.green[18]),
-    Magenta: r(colors.purple[18]),
-    Red: r(colors.red[18]),
-    White: r(colors.blueGray[26]),
-    Yellow: r(colors.orange[26])
+    Green: r(colors.green[26]),
+    Magenta: r(colors.purple[26]),
+    Red: r(colors.red[26]),
+    White: r(colors.blueGray[34]),
+    Yellow: r(colors.orange[34])
   };
 }
 
