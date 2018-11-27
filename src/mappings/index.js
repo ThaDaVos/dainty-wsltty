64eebf927cb98d23692ba1b432940608a11a84c4
Alexander Teinum <ateinum@gmail.com>
2018-11-28 00:48:27 +0100

    Use lighter shades for all colors except non-bold red and non-bold green

--- src/mappings/index.js
+++ src/mappings/index.js
@@ -10,23 +10,23 @@ function getMappings(colors) {
   return {
     BackgroundColour: r(colors.blueGray[0]),
     Black: r(colors.blueGray[0]),
-    Blue: r(colors.blue[26]),
-    BoldBlack: r(colors.blueGray[26]),
-    BoldBlue: r(colors.blue[26]),
-    BoldCyan: r(colors.cyan[26]),
-    BoldGreen: r(colors.green[26]),
-    BoldMagenta: r(colors.purple[26]),
-    BoldRed: r(colors.red[26]),
+    Blue: r(colors.blue[28]),
+    BoldBlack: r(colors.blueGray[28]),
+    BoldBlue: r(colors.blue[28]),
+    BoldCyan: r(colors.cyan[28]),
+    BoldGreen: r(colors.green[28]),
+    BoldMagenta: r(colors.purple[28]),
+    BoldRed: r(colors.red[28]),
     BoldWhite: r(colors.blueGray[39]),
-    BoldYellow: r(colors.yellow[26]),
+    BoldYellow: r(colors.yellow[28]),
     CursorColour: r(colors.blueGray[28]),
-    Cyan: r(colors.cyan[26]),
+    Cyan: r(colors.cyan[28]),
     ForegroundColour: r(colors.blueGray[34]),
     Green: r(colors.green[16]),
-    Magenta: r(colors.purple[26]),
-    Red: r(colors.red[20]),
+    Magenta: r(colors.purple[28]),
+    Red: r(colors.red[16]),
     White: r(colors.blueGray[34]),
-    Yellow: r(colors.yellow[26])
+    Yellow: r(colors.yellow[28])
   };
 }
 
